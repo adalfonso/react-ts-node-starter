c69fdcf698e91d28d419e5d8fc0e046d55b36334
Anthony Dalfonso <anthonyjdalfonso@gmail.com>
2022-04-17 22:29:19 -0400

    fix: Fix HMR for node server code with docker

--- configs/webpack/server-dev.config.js
+++ configs/webpack/server-dev.config.js
@@ -1,4 +1,10 @@
 const common = require("./server-common.config");
 const { merge } = require("webpack-merge");
 
-module.exports = merge(common, { mode: "development" });
+module.exports = merge(common, {
+  mode: "development",
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+});
